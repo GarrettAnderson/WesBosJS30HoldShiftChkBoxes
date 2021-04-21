48a89ae03ce31a0870e516ffdc0950f91134e795
GarrettAnderson <garrettleegrahamanderson@gmail.com>
2021-04-21 19:51:07 -0400

    setting up the project to begin writing the JS. began with a variable to target the checkbox input element.

---
+++ main.js
@@ -0,0 +1,67 @@
+// MY SOLUTION
+
+
+
+
+const chkBox = document.querySelector('input[type="checkbox"]')
+
+console.log(chkBox)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// WES BOS SOLUTION
+
+//
+//
+// const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
+//
+// let lastChecked;
+//
+// function handleCheck(e) {
+//   console.log(e)
+//   console.log(this)
+//   // Check if they had the shift key down
+//   // AND check that they are checking it
+//   let inBetween = false;
+//   if (e.shiftKey && this.checked) {
+//     // go ahead and do what we please
+//     // loop over every single checkbox
+//     checkboxes.forEach(checkbox => {
+//       console.log(checkbox);
+//       if (checkbox === this || checkbox === lastChecked) {
+//         inBetween = !inBetween;
+//         console.log('Starting to check them in between!');
+//       }
+//
+//       if (inBetween) {
+//         checkbox.checked = true;
+//       }
+//     });
+//   }
+//   lastChecked = this;
+// }
+//
+// checkboxes.forEach(checkbox => checkbox.addEventListener('click', handleCheck));
